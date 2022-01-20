16e708ee1e640bf75de282245bf9c509e44a408d
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2022-01-20 14:26:36 -0300

    initial commit

---
+++ apps/packetriot-client-gui/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'packetriot-client-gui',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/packetriot-client-gui',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
